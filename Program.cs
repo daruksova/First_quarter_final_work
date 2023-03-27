ade2b2551bc5d5c762521a4e20343620af0ee483
Daria <ruksova17@gmail.com>
2023-03-27 12:24:33 +0400

    Add_method_counting_elemets_longer3

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с 
 // клавиатуры, либо задать на старте выполнения алгоритма. 
 
+// Записывает введенный с клавиатуры массив строк
 string[] ReadString()
 {
     Console.Write("Введите исходный массив строк: ");
@@ -9,10 +10,26 @@ string[] ReadString()
     return(arr);
 }
 
+// Выводит массив в консоль
 void PrintArray(string[] arr)
 {
     Console.WriteLine("[" + String.Join(", ", arr) + "]");
 }
 
+// Считает количество элементов массива длиной <= 3 символа
+int ResultArrayLength(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 string[] array = ReadString();
 PrintArray(array);
+System.Console.WriteLine(ResultArrayLength(array));
